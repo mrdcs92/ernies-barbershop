1bd1a6369c1ade23eb079b6c0d86f3d8f4def72f
Dylan Smith <mrdcs92@gmail.com>
2018-03-20 13:59:53 -0700

    4th test

--- js/app.js
+++ js/app.js
@@ -26,37 +26,37 @@
         $routeProvider.when("/", {
             controller: "homeController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/homeView.html"
+            templateUrl: "/js/views/homeView.html"
         });
 
         $routeProvider.when("/about", {
             controller: "homeController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/aboutView.html"
+            templateUrl: "/js/views/aboutView.html"
         });
 
         $routeProvider.when("/bookings", {
             controller: "homeController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/bookView.html"
+            templateUrl: "/js/views/bookView.html"
         });
 
         $routeProvider.when("/blog", {
             controller: "blogController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/blogView.html"
+            templateUrl: "/js/views/blogView.html"
         });
 
         $routeProvider.when("/login", {
             controller: "authController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/authView.html"
+            templateUrl: "/js/views/authView.html"
         });
 
         $routeProvider.when("/edit", {
             controller: "editController",
             controllerAs: "vm",
-            templateUrl: ".../js/views/editView.html",
+            templateUrl: "/js/views/editView.html",
             authenticated: true
         });
 
